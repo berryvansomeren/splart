Fixed bugs in sampling and scaling

splart/grow.py
@@ -1,25 +1,25 @@
 import numpy as np
 import torch
 
-from config import TrainingConfig
-from splat_components import in_place_pack_into_splat_tensor_starting_from
+from splart.training_config import TrainingConfig
+from splart.splat_components import in_place_pack_into_splat_tensor_starting_from
 
 
 def get_brush_scale_for_l1_loss(config: TrainingConfig, current_l1_loss: float, current_epoch: int) -> float:
     # loss is assumed to be a l1 loss
     # by multiplying with N we turn the MSE into a sum of absolute differences
     # this can then be turned into a percentage, which is used to determine the brush size
-    n_pixels = np.prod(config.target_image_load_size)
+    n_pixels = np.prod(config.target_image_load_size.as_w_h())
     diff_score = current_l1_loss * n_pixels
     max_potential_diff_score = n_pixels  # pixel values are normalized otherwise we had to * 255
     normalized_diff_score = diff_score / max_potential_diff_score
 
     # use the normalized diff score to determine a desired brush size
-    image_min_extend = min(config.target_image_load_size[:2])
+    image_min_extend = min(config.target_image_load_size.as_w_h())
     desired_brush_size = int(image_min_extend * normalized_diff_score)
 
     # determine the scale required to achieve the desired brush size
-    scale = desired_brush_size / config.texture_load_size
+    scale = desired_brush_size / config.texture_load_size.width  # width or height does not matter here, they are equal
     assert scale <= 1.0, f"Scale {scale} is larger than 1.0. This results in NaNs. Choose a higher texture_load_size!"
     return scale
 
@@ -38,13 +38,14 @@ def in_place_add_samples_starting_from(
     # But we are going to use them in grid_sample, which expects x, y,
     #  and an NDC like coordinate space with y down
     # Let's already use the x, y order here.
-    target_wh = config.target_image_load_size[1::-1]  # w, h
-    new_positions_absolute = np.random.randint(0, target_wh, size=(n_samples, 2))  # x, y
-    new_positions_unit_normalized = torch.tensor(new_positions_absolute / target_wh, device=config.device).float()
-    new_positions_ndc = (new_positions_unit_normalized * 2.0) - 1.0
+    target_w_h = config.target_image_load_size.as_w_h()  # w, h
+    new_positions_absolute_xy = np.random.randint(0, target_w_h, size=(n_samples, 2))  # x, y
+    new_positions_unit_normalized = torch.tensor(new_positions_absolute_xy / target_w_h, device=config.device).float()
+    new_positions_ndc = 2 * new_positions_unit_normalized - 1  # map to [-1, 1]
 
-    # Fetching the colour of the pixels in each coordinates
-    new_colors_np = np.array([target_image_np[coord[1], coord[0]] for coord in new_positions_absolute])
+    # Fetching the colour of the pixels at each coordinate pair
+    # numpy expects y,x!
+    new_colors_np = np.array([target_image_np[y, x] for x, y in new_positions_absolute_xy])
     new_colors_tensor = torch.tensor(new_colors_np, device=config.device).float()
 
     # Scales based on current loss -> lower loss means adding more fine-grained details

splart/renderer.py
@@ -1,12 +1,13 @@
 import torch
 import numpy as np
-from splat_components import unpack, Columns
+from splart.splat_components import unpack, Columns
+from splart.training_config import Size
 
 
-def extend_texture(image_size, batch_size, texture):
+def extend_texture(image_size: Size, batch_size: int, texture: torch.Tensor) -> torch.Tensor:
     # Ensure padding to match image size
-    pad_h = image_size[0] - texture.shape[1]  # image_size = H, W
-    pad_w = image_size[1] - texture.shape[2]  # texture.shape = C, H, W
+    pad_h = image_size.height - texture.shape[1]  # image_size = H, W
+    pad_w = image_size.width - texture.shape[2]  # texture.shape = C, H, W
     padding = (pad_w // 2, pad_w // 2 + pad_w % 2, pad_h // 2, pad_h // 2 + pad_h % 2)
     texture_padded = torch.nn.functional.pad(texture, padding, "constant", 0)  # Pad
 
@@ -52,36 +53,54 @@ def render_2d_texture_splatting(
     image_size,
     device: torch.device,
 ):
+    # We separate scaling and rotation, from translation
+    # SO that it is much easier to prevent them from affecting each other
+    # This also makes debugging a lot easier.
+    # Combining everything in a single sample grid is very error-prone, perhaps even impossible
+
+    # Unpack splat components
     scale = unpack(splat_weights, Columns.scales)
     rotation = np.pi / 2 * unpack(splat_weights, Columns.rotations)
     colors = unpack(splat_weights, Columns.colors)
-    coords = unpack(splat_weights, Columns.positions)
+    positions = unpack(splat_weights, Columns.positions)
 
     batch_size = colors.shape[0]
     splat_textures = extend_texture(image_size, batch_size, texture)
     _, n_channels, height, width = splat_textures.shape
 
-    # Create the scaling and rotation matrix
+    # Step 1: Rotation and Scaling
     cos_rot = torch.cos(rotation)
     sin_rot = torch.sin(rotation)
     rotation_matrix = torch.stack([cos_rot, -sin_rot, sin_rot, cos_rot], dim=-1).view(batch_size, 2, 2)
-    scale_matrix = torch.diag_embed(1.0 / scale.view(-1, 1).expand(-1, 2))
-    affine_matrix = rotation_matrix @ scale_matrix
+    # In case of rectangular images, the normalized coordinate system would stretch our textures
+    # We correct the aspect ratio through heterogeneous scaling
+    # We scale relative to the smallest image dimension
+    min_dim = min(width, height)
+    aspect_correction = torch.tensor([width / min_dim, height / min_dim], device=device).view(1, 2)
+    scale_matrix = torch.diag_embed(1.0 / scale.view(-1, 1).expand(-1, 2) * aspect_correction)
+    scaling_rotation_matrix = torch.zeros(batch_size, 2, 3, dtype=torch.float32, device=device)
+    scaling_rotation_matrix[:, :2, :2] = rotation_matrix @ scale_matrix  # Apply rotation and scaling
+    scaling_rotation_grid = torch.nn.functional.affine_grid(
+        theta=scaling_rotation_matrix, size=list(splat_textures.shape), align_corners=True
+    )
+    scaled_rotated_textures = torch.nn.functional.grid_sample(
+        input=splat_textures, grid=scaling_rotation_grid, align_corners=True
+    )
 
-    # Pre-scale the translation to counteract scaling
-    adjusted_translation = (-coords) / scale.view(-1, 1)
-
-    # Construct the final affine transformation matrix
-    final_affine_matrix = torch.zeros(batch_size, 2, 3, dtype=torch.float32, device=device)
-    final_affine_matrix[:, :2, :2] = affine_matrix
-    final_affine_matrix[:, :, 2] = adjusted_translation
-
-    # Apply the transformation in a single step
-    grid = torch.nn.functional.affine_grid(final_affine_matrix, list(splat_textures.shape), align_corners=True)
-    transformed_textures = torch.nn.functional.grid_sample(splat_textures, grid, align_corners=True)
+    # Step 2: Translation
+    translation_matrix = torch.zeros(batch_size, 2, 3, dtype=torch.float32, device=device)
+    translation_matrix[:, 0, 0] = 1.0
+    translation_matrix[:, 1, 1] = 1.0
+    translation_matrix[:, :, 2] = -positions
+    translating_grid = torch.nn.functional.affine_grid(
+        theta=translation_matrix, size=list(splat_textures.shape), align_corners=True
+    )
+    translated_textures = torch.nn.functional.grid_sample(
+        input=scaled_rotated_textures, grid=translating_grid, align_corners=True
+    )
 
     final_image = combine_layers_using_color_and_alpha(
-        splat_layers=transformed_textures,
+        splat_layers=translated_textures,
         colors=colors,
     )
     return final_image

splart/run_optimization.py
@@ -9,12 +9,11 @@ from PIL import Image
 import random
 import matplotlib.pyplot as plt
 
-from config import load_training_config, TrainingConfig
-from grow import in_place_add_samples_starting_from
-from loss_perturbation import get_perturbed_loss
-from renderer import render_2d_texture_splatting
-from splat_components import init_empty_splat_weights
-from make_gif import write_gif_for_folder
+from splart.training_config import TrainingConfig
+from splart.grow import in_place_add_samples_starting_from
+from splart.renderer import render_2d_texture_splatting
+from splart.splat_components import init_empty_splat_weights, unpack, Columns
+from splart.make_gif import write_gif_for_folder
 
 
 def set_random_seeds() -> None:
@@ -26,19 +25,16 @@ def set_random_seeds() -> None:
 
 
 def _load_target_image(config: TrainingConfig) -> tuple[torch.Tensor, np.ndarray]:
-    image_file = (
-        Image.open(config.target_image_file_path)
-        .resize((config.target_image_load_size[1], config.target_image_load_size[0]))
-        .convert("RGB")
-    )
+    # PIL uses W,H order!
+    image_file = Image.open(config.target_image_path).resize(config.target_image_load_size.as_w_h()).convert("RGB")
     target_image_np = np.array(image_file) / 255.0
     target_image_tensor = torch.tensor(target_image_np, dtype=torch.float32, device=config.device)
     return target_image_tensor, target_image_np
 
 
 def _load_texture(config: TrainingConfig) -> torch.Tensor:
-    texture_size_2d = (config.texture_load_size, config.texture_load_size)
-    texture_file = Image.open(config.texture_image_file_path).convert("RGB").resize(texture_size_2d)
+    # PIL uses W,H order!
+    texture_file = Image.open(config.texture_image_path).resize(config.texture_load_size.as_w_h()).convert("RGB")
     texture_np = np.array(texture_file)  # Shape: (H, W, 3)
     red_channel_np = texture_np[..., 0] / 255.0  # Extract Red channel (H, W)
     texture_tensor = torch.tensor(red_channel_np, dtype=torch.float32, device=config.device)
@@ -93,7 +89,19 @@ def init_persistent_mask(config: TrainingConfig) -> torch.Tensor:
     return persistent_mask
 
 
-def main(config: TrainingConfig) -> None:
+def get_perturbed_loss(
+    current_loss: float,
+    current_epoch: int,
+    n_loss_perturbation_epochs: int,
+    expected_loss_after_perturbation_epochs: float,
+) -> float:
+    current_epoch_t: float = current_epoch / n_loss_perturbation_epochs
+    current_linear_loss: float = (1 - current_epoch_t) * 1 + current_epoch_t * expected_loss_after_perturbation_epochs
+    current_perturbed_loss: float = (1 - current_epoch_t) * current_linear_loss + current_epoch_t * current_loss
+    return current_perturbed_loss
+
+
+def run_optimization(config: TrainingConfig) -> None:
     run_start = datetime.now()
     set_random_seeds()
 
@@ -103,6 +111,10 @@ def main(config: TrainingConfig) -> None:
 
     target_image, target_image_np = _load_target_image(config)
     texture = _load_texture(config)
+
+    scaled_input_image_path = os.path.join(output_folder, "input_image.png")
+    print(f"Writing scaled input image to {scaled_input_image_path}")
+    _write_image(image_array=target_image_np, image_path=scaled_input_image_path)
 
     current_l1_loss_value = 1.0
     perturbed_loss = 1.0
@@ -192,16 +204,37 @@ def main(config: TrainingConfig) -> None:
         if do_pruning:
             # Prune splats with low gradients
             # They will still be rendered, but no longer optimized!
+            # This is intended to stop optimizing splats that already have near-optimal components
+            # Of course later splats could change the situation again,
+            # but it's nice to allow some pruning for the sake of performance,
+            # and to reinforce the "painting" effect, where older paint is no longer changed
+            # once new paint is applied on top
             gradient_norms = torch.norm(splat_weights.grad, dim=1, p=2)
             gradient_norms_small_bools_non_masked = gradient_norms < config.pruning_gradient_threshold
             gradient_norms_small_bools = gradient_norms_small_bools_non_masked & samples_optimization_mask
             indices_to_remove_based_on_gradient = gradient_norms_small_bools.nonzero(as_tuple=True)[0]
             if len(indices_to_remove_based_on_gradient) > 0:
-                print(f"Pruned {len( indices_to_remove_based_on_gradient )} points based on gradients")
+                print(f"Pruned {len( indices_to_remove_based_on_gradient )} points based on Gradient Norm")
             samples_optimization_mask[indices_to_remove_based_on_gradient] = False
 
+            # Prune splats with low scales
+            # They will no longer be optimized and no longer be rendered!
+            # This is intended to remove points
+            # where the optimizer tries to get rid of bad points,
+            # by reducing their scale
+            scale = unpack(splat_weights, Columns.scales).squeeze()
+            scale_small_bools_non_masked = scale < config.pruning_scale_threshold
+            scale_small_bools = scale_small_bools_non_masked & samples_rendering_mask
+            indices_to_remove_based_on_scale = scale_small_bools.nonzero(as_tuple=True)[0]
+            if len(indices_to_remove_based_on_scale) > 0:
+                print(f"Pruned {len( indices_to_remove_based_on_scale )} points based on Scale")
+            samples_optimization_mask[indices_to_remove_based_on_scale] = False
+            samples_rendering_mask[indices_to_remove_based_on_scale] = False
+
         # Every epoch, zero-out the gradients of pruned points.
-        # This should speed up the optimization step, but you won't notice it much, as rendering is the bottleneck
+        # This should speed up the optimization step, but you won't notice it much,
+        # as rendering is a severe bottleneck, not much impacted by the pruning
+        # Make sure this happens after computing gradients, and before optimization
         if current_epoch > 0:
             # We use a cast to satisfy mypy, we know grad is not None
             cast(torch.Tensor, splat_weights.grad).data[~samples_optimization_mask] = 0.0
@@ -226,18 +259,10 @@ def main(config: TrainingConfig) -> None:
                 f"Loss: {current_l1_loss_tensor:.3f}, "
             )
             if config.log_loss_perturbation and do_growth and current_epoch < config.n_loss_perturbation_epochs:
-                message += f" - Loss of previous epoch was perturbed to {perturbed_loss:.3f}."
+                message += f"- Previous Loss perturbed to {perturbed_loss:.3f}."
             print(message)
 
     # Postprocessing
     print(f"Final Loss: {current_l1_loss_tensor}")
     write_loss_graphs(loss_history=loss_history, output_folder=output_folder)
     write_gif_for_folder(output_folder=output_folder)
-
-
-if __name__ == "__main__":
-    main_start_time = time.perf_counter()
-    main(config=load_training_config())
-    main_end_time = time.perf_counter()
-    main_elapsed_time = main_end_time - main_start_time
-    print(f"Done! Took: {main_elapsed_time:.6f} seconds")